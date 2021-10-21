87eb6802d0adb4b6ce759da2936a971f8c379c44
Ronald Shutter <r.a.shutter.com>
2021-10-21 12:19:28 -0700

    updated to server-jpgs

--- server.js
+++ server.js
@@ -5,26 +5,24 @@ const mongoose = require("mongoose");
 // const logger = require("morgan");
 
 
-
-
-// default port information - set port or get default of 3001
+// default port information - set port or get default of 3000
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-
+// Database login  --------------
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 
-app.use(require("./routes/apiRoutes"));
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 // the new route
-// app.use(require("./routes/"));
+app.use(require("./routes/apiRoutes"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on: http://localhost:${PORT}`);
